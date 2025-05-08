5a6ed1795f5f09e41a11c0ced646269e16cde351
Jin Kim <jinkim@Jins-MBP.home>
2025-05-07 22:50:30 -0400

    chore: cleaned up and added print statements to functions

--- src/app.py
+++ src/app.py
@@ -6,21 +6,22 @@ from langchain.vectorstores import Chroma
 
 def test_embed_subset(category):
     chunks = load_category_data(category)
-    chunks_subset = [chunks[x] for x in range(10)]
-    embed_store_chunks(chunks_subset)
+    embed_store_chunks(chunks)
+
+def query_db(query):
+    embedding_fn = get_embedding_fn()
+    vector_store = Chroma(
+        persist_directory="chroma_db/",  # must match what you used when saving
+        embedding_function=embedding_fn
+    )
+    results = vector_store.similarity_search(query, k=3)
+    for i, doc in enumerate(results, 1):
+        print(f"\n--- Result {i} ---")
+        print("Content:", doc.page_content)
+        print("Metadata:", doc.metadata)
 
 
-embedding_fn = get_embedding_fn()
-vector_store = Chroma(
-    persist_directory="chroma_db/",  # must match what you used when saving
-    embedding_function=embedding_fn
-)
-query = "What sword has the highest physical attack?"
-results = vector_store.similarity_search(query, k=3)
-for i, doc in enumerate(results, 1):
-    print(f"\n--- Result {i} ---")
-    print("Content:", doc.page_content)
-    print("Metadata:", doc.metadata)
+test_embed_subset('Weapons')
 
 
 
--- src/embedding.py
+++ src/embedding.py
@@ -1,18 +1,21 @@
 from langchain.embeddings import OpenAIEmbeddings
 from dotenv import load_dotenv
 from langchain.vectorstores import Chroma
-
+import os
 
 
 
 def get_embedding_fn():
     load_dotenv()
+    assert os.getenv("OPENAI_API_KEY"), "API key not loaded!"
     return OpenAIEmbeddings(model="text-embedding-3-small")
 
 def embed_store_chunks(chunks):
     embedding_fn = get_embedding_fn()
+    texts = [chunk.get("chunk") for chunk in chunks]
+    print(f"Embedding {len(texts)} chunks")
     vector_store = Chroma.from_texts(
-        texts=[chunk.get("chunk") for chunk in chunks],
+        texts=texts,
         embedding=embedding_fn,
         metadatas=[chunk.get("metadata") for chunk in chunks],
         ids=[chunk.get("id") for chunk in chunks],
